3d4f24b071883771907fd866862a1225398ca540
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-04-12 12:12:55 -0300

    HW D4v2

---
+++ front/scripts/functions.js
@@ -0,0 +1,49 @@
+function mapMovies(data) {
+    const container = document.getElementById("movieContainer");
+
+    const movies = getMovies(data);
+    movies.map(movie => createMovieCard(movie))
+    .forEach(card => container.appendChild(card));
+}
+
+function getMovies(data) {
+    return data ;
+}
+
+function createMovieCard(data) {
+    const {title, year, director, duration, genre, rate, poster} = data ;
+
+    const movieCard = document.createElement("div");
+    const movieTitle = document.createElement("a");
+    const movieYear = document.createElement("p");
+    const movieDirector = document.createElement("p");
+    const movieDuration = document.createElement("p");
+    const movieGenre = document.createElement("p");
+    const movieRate = document.createElement("p");
+    const moviePoster = document.createElement("img");
+
+    movieTitle.innerHTML = title ; 
+    movieYear.innerHTML = `Año: ${year}`; 
+    movieDirector.innerHTML = `Director: ${director}`; 
+    movieDuration.innerHTML = `Duración: ${duration}`; 
+    movieGenre.innerHTML = `Género: ${genre}`; 
+    movieRate.innerHTML = `Puntaje: ${rate}`; 
+    moviePoster.src = poster ; 
+
+    movieCard.classList.add("cards"); 
+    moviePoster.classList.add("poster"); 
+    movieTitle.classList.add("tituloPelicula")
+    movieTitle.classList.add("etiqA")
+
+    movieCard.appendChild(movieTitle) ; 
+    movieCard.appendChild(moviePoster) ; 
+    movieCard.appendChild(movieYear) ; 
+    movieCard.appendChild(movieDirector) ; 
+    movieCard.appendChild(movieDuration) ; 
+    movieCard.appendChild(movieGenre) ; 
+    movieCard.appendChild(movieRate) ; 
+
+    return movieCard;
+}
+
+module.exports = mapMovies;
