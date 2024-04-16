04cf06b54a20b09dbc059370e4cc4dc66117f42c
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-04-16 19:19:30 -0300

    HW D7

---
+++ back/index.js
@@ -0,0 +1,3 @@
+const app = require("./src/server")
+
+app.listen(3000, () => console.log("La app esta lista en el puerto 3000")) ;
---
+++ back/src/controllers/moviesController.js
@@ -0,0 +1,5 @@
+const moviesController = (req , res) => {
+    res.status(200).send("Proximamente estarán disponibles los datos de las películas.")
+}
+
+module.exports = moviesController ;
