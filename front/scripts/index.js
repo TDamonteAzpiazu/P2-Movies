fec060f088c8652e32c20b025d7739952e21977d
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-04-15 22:36:05 -0300

    HW D6

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,16 @@
 const mapMovies = require("./functions.js")
+const axios = require ("axios")
 
-$.get("https://students-api.up.railway.app/movies" , (data) => mapMovies(data)).fail(() => alert("Error al obtener las películas"))
+// $.get("https://students-api.up.railway.app/movies" , (data) => mapMovies(data)).fail(() => alert("Error al obtener las películas"))
 
+async function getData() {
+    try {
+        const data = await axios.get("https://students-api.up.railway.app/movies");
+        mapMovies(data.data) ;
+    } catch (error) {
+        alert("Error al obtener las películas.")
+    }
+}
 
+getData();
 
