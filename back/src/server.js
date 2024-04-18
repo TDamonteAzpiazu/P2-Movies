35b0a6e5403eecf56bc84fc2ef39d3ef6ebdcfa1
TDamonteAzpiazu <tobidamonte@gmail.com>
2024-04-18 12:13:33 -0300

    HW D8 v2

--- back/src/server.js
+++ back/src/server.js
@@ -5,7 +5,7 @@ const app = express() ;
 
 const morgan = require("morgan")
 const cors = require("cors")
-app.use(morgan("dev"))
+app.use(morgan("tiny"))
 app.use(cors())
 app.use(express.json())
 
--- back/src/services/moviesService.js
+++ back/src/services/moviesService.js
@@ -1,53 +1,54 @@
-class Movie {
-    constructor(title, year, director, duration, genre, rate, poster) {
-        if (!title || !year || !director || !duration || !genre || !rate || !poster) {
-            throw new Error("Todos los campos son necesarios")
-        }
-
-        this.title = title ;
-        this.year = year ;
-        this.director = director ;
-        this.duration = duration ;
-        this.genre = genre ;
-        this.rate = rate ;
-        this.poster = poster ;
-    }
-}
+const Movie = require ("../util/classMovie")
 
 let movies = [
-    new Movie(
-        "Guardians of the Galaxy Vol. 2",
-        2017,
-        "James Gunn",
-        "2h 16min",
-        ["Action", "Adventure", "Comedy" ],
-        7.7,
-        "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
-    ),
-    new Movie (
-        "Star Wars: Episode IV - A New Hope",
-        1977,
-        "George Lucas",
-        "2h 1min",
-        ["Action", "Adventure", "Fantasy", "Sci-Fi" ],
-        8.7,
-        "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
-    ),
-    new Movie(
-        "The Lord of the Rings: The Fellowship of the Ring",
-        2001,
-        "Peter Jackson",
-        "2h 58min",
-        ["Action", "Adventure", "Drama", "Fantasy" ],
-        8.8,
-        "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
-    )
-    
+
+    {
+        "title": "Guardians of the Galaxy Vol. 2",
+        "year": 2017,
+        "director": "James Gunn",
+        "duration": "2h 16min",
+        "genre": [
+          "Action",
+          "Adventure",
+          "Comedy"
+        ],
+        "rate": 7.7,
+        "poster": "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"
+    },
+    {
+      "title": "Star Wars: Episode IV - A New Hope",
+      "year": 1977,
+      "director": "George Lucas",
+      "duration": "2h 1min",
+      "genre": [
+        "Action",
+        "Adventure",
+        "Fantasy",
+        "Sci-Fi"
+      ],
+      "rate": 8.7,
+      "poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+    },
+    {
+      "title": "The Lord of the Rings: The Fellowship of the Ring",
+      "year": 2001,
+      "director": "Peter Jackson",
+      "duration": "2h 58min",
+      "genre": [
+        "Action",
+        "Adventure",
+        "Drama",
+        "Fantasy"
+      ],
+      "rate": 8.8,
+      "poster": "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
+    }
   ]
 
 
   module.exports = {
     getMovies : async (req, res) => {
-        return movies
+        const newMovie = movies.map(movie => new Movie(movie))
+        return newMovie
     }
 }
